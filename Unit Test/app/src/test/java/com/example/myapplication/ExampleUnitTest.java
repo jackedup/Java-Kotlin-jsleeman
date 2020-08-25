1603d3545fa6f5dc0be89dfa2dade53ceb9deda3
Jack Sleeman <jsleeman1@icloud.com>
2020-08-25 11:31:38 -0600

    added subtraction to unit test

--- Unit Test/app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ Unit Test/app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -18,4 +18,6 @@ public class ExampleUnitTest {
     public void concattenation_isCorrect() {assertEquals("Hello m8", "Hello" + " m8");}
     @Test
     public void stringtoint_isCorrect(){assertEquals(Integer.parseInt("1"), 1);}
+    @Test
+    public void subtract_isCorrect(){assertEquals(4,8-4);}
 }
