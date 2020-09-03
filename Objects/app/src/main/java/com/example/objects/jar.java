a602ef8ca492380d529038b78361e379adb08f71
Jack Sleeman <jsleeman1@icloud.com>
2020-09-03 12:03:46 -0600

    added Objects initial commit

---
+++ Objects/app/src/main/java/com/example/objects/jar.java
@@ -0,0 +1,39 @@
+package com.example.objects;
+
+public class jar {
+
+    public jar(double capacity){
+        if (capacity > 0) {
+            this.capacity = capacity;
+        }else {
+            String message = "Capacity: " + capacity + " is not greater than 0.";
+            throw new IllegalArgumentException(message);
+        }
+    }
+    private double capacity;
+    public double getCapacity(){
+        return capacity;
+    }
+
+    private double fluidLevel;
+    public double getFluidLevel(){
+        return fluidLevel;
+    }
+    public void setFluidLevel(double fluidLevel){
+        if (0 < fluidLevel && fluidLevel < this.capacity){
+            this.fluidLevel = fluidLevel;
+        }else{
+            String message = "fluidLevel: " + fluidLevel + " is not between 0 and " + this.capacity;
+            throw new IllegealArgumentMessage(message);
+        }
+    }
+
+    public boolean isLidOn;
+    public boolean getLidOn(){
+        return isLidOn;
+    }
+    public void setLidOn(boolean isLidOn){
+        this.isLidOn = isLidOn;
+    }
+
+}
---
+++ Objects/app/src/test/java/com/example/objects/jarTest.java
@@ -0,0 +1,28 @@
+package com.example.objects;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class jarTest {
+
+    @Test
+    public void getCapacity() {
+    }
+
+    @Test
+    public void getFluidLevel() {
+    }
+
+    @Test
+    public void setFluidLevel() {
+    }
+
+    @Test
+    public void getLidOn() {
+    }
+
+    @Test
+    public void setLidOn() {
+    }
+}
